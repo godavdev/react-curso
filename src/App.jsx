1ed308f0fec68a4a80e359052bb26ef88ffe9763
godav <dgaduff@gmail.com>
2024-02-06 10:12:30 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="w-full h-screen bg-slate-900 flex justify-center items-center">
+      <div className="w-96 h-96 bg-white"></div>
+    </div>
+  )
+}
+
+export default App
